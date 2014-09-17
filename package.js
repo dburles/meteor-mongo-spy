724a4b2d97c01b90bd45b948ea3a3c03532a755c
David Burles <dburles@gmail.com>
2014-09-17 19:38:06 +1000

    update package.js

--- package.js
+++ package.js
@@ -6,13 +6,13 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.1.1');
-  api.use(['mongo', 'underscore'], 'server');
+  api.use(['mongo@1.0.4', 'underscore@1.0.0'], 'server');
   api.addFiles('mongo-spy.js', 'server');
   api.export('MongoSpy', 'server');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('mongo-spy');
+  api.use('dburles:mongo-spy');
   api.addFiles('mongo-spy-tests.js');
 });
